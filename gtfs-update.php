4955a8dfa7de9602e60bf5c8c00da57931c2d09b
Claudette Raynor <sleddd@users.noreply.github.com>
2020-08-21 11:34:12 -0700

    Some small code style changes

--- gtfs-update.php
+++ gtfs-update.php
@@ -7,8 +7,7 @@ function the_gtfs_update_form() {
 	?>
 	<style type="text/css">#wpfooter {display: none;}</style>
 	<h2>GTFS Site Update</h2>
-	<p>
-	GTFS update will automatically create and update route pages (if active) and timetables (if the optional <em>timetables.txt</em> file is included in the feed). Do not perform an update if you are not sure what you are doing.
+	<p>GTFS update will automatically create and update route pages (if active) and timetables (if the optional <em>timetables.txt</em> file is included in the feed). Do not perform an update if you are not sure what you are doing.
 	Performing GTFS update will automatically download the most recent version of your feed from the given feed URL.<br />
 	For more information see the <a href="https://trilliumtransit.github.io/transit-custom-posts/gtfs-update.html">GTFS Update usage guide</a>.
 	</p>
@@ -67,8 +66,7 @@ function the_gtfs_update_form() {
 									<input type="file" id="timetable_zip_input" name="timetable_zip_input" accept="application/zip,application/x-zip,application/x-zip-compressed" />
 								</label>
 							</p>
-							<p class="description">If no timetables are provided, GTFS Update will still update routes as well as any changes to timetable fields present in <em>timetables.txt</em>.
-							Previously uploaded timetable HTML will only be overwritten if new timetables are uploaded.</p>
+							<p class="description">If no timetables are provided, GTFS Update will still update routes as well as any changes to timetable fields present in <em>timetables.txt</em>. Previously uploaded timetable HTML will only be overwritten if new timetables are uploaded.</p>
 						</fieldset>
 					</td>
 				</tr>
@@ -81,45 +79,40 @@ function the_gtfs_update_form() {
 		</p>
 	</form>
 	<?php
-	// TODO: refactor this to be less repetitive, change error submission
-	if(isset($_GET['submit_status'])) {  //
-		$status_code = $_GET['submit_status'];
-		if ( intval($status_code) < 200 ) {
-			echo '<div id="setting-error-settings_updated" class="error settings-error notice is-dismissible"><p>Submission Error: ';
-			echo tcp_get_status_message( $status_code );
-			echo '</p></div>';
-		} else {
-			echo '<div id="setting-error-settings_updated" class="updated settings-error notice is-dismissible"><p>';
+		if ( isset( $_GET['submit_status'] ) ) { 
+			$status_code = $_GET['submit_status'];
+			$error_msg   = intval($status_code) < 200 ? 'Submission Error: ' : '';
+			$error_class = intval($status_code) < 200 ? 'error' : 'updated';
+			echo '<div id="setting-error-settings_updated" class="' . $error_class . ' settings-error notice is-dismissible"><p>' . $error_msg;
 			echo tcp_get_status_message( $status_code );
 			echo '</p></div>';
 		}
-	}
-	echo '</div>';
+		echo '</div>';
 }
 add_action( 'admin_action_tcp_gtfs_update','tcp_gtfs_update' );
 
 function tcp_gtfs_update() {
 
     // Ensure request came from correct screen
-    if ( !wp_verify_nonce( $_POST['gtfsupdate_noncename'], 'gtfs-update' )) {
+    if ( ! wp_verify_nonce( $_POST['gtfsupdate_noncename'], 'gtfs-update' ) ) {
 		tcp_status_redirect('100');
     }
     // Ensure user has Admin capabilities
-    if ( !current_user_can( 'update_core')) {
+    if ( ! current_user_can( 'update_core') ) {
 		tcp_status_redirect('101');
     }
     // Ensure backup was checked
-	if(! isset($_POST['backup'])) {
+	if ( ! isset( $_POST['backup'] ) ) {
 		tcp_status_redirect('102');
 	}
     // Ensure this theme is actually using custom Route types
-	if ( !post_type_exists( 'route' ) ) {
+	if ( ! post_type_exists( 'route' ) ) {
 		tcp_status_redirect('103');
 	}
 
 	$feed_path = tcp_download_feed();
 
-    if ( !$feed_path ) {
+    if ( ! $feed_path ) {
 		tcp_status_redirect('104');
     }
 
@@ -129,14 +122,15 @@ function tcp_gtfs_update() {
 		$routes_txt = apply_filters('tcp_filter_route_txt_path', $routes_txt );
 	} 
 
-	if ( !file_exists( $routes_txt) ) {
+	if ( ! file_exists( $routes_txt) ) {
        tcp_status_redirect('104');
 	}
 
-	if ( !($res = tcp_update_routes($routes_txt)) ) {
+	if ( ! ( $res = tcp_update_routes( $routes_txt ) ) ) {
 		tcp_status_redirect('104');
 	}
-	if ( !post_type_exists( 'timetable' ) ) {
+
+	if ( ! post_type_exists( 'timetable' ) ) {
 		tcp_status_redirect('200');
 	}
 
@@ -149,10 +143,10 @@ function tcp_gtfs_update() {
 		$timetables_txt = apply_filters('tcp_filter_timetables_txt_path', $timetables_txt );
 	} 
 
-	if ( !file_exists( $timetables_txt ) ) {
+	if ( ! file_exists( $timetables_txt ) ) {
 		tcp_status_redirect('201');
 	}
-	if ( !($res = tcp_update_timetables($timetables_txt)) ) {
+	if ( ! ( $res = tcp_update_timetables( $timetables_txt ) ) ) {
 		tcp_status_redirect('201');
 	}
 
@@ -165,7 +159,7 @@ function tcp_gtfs_update() {
 
 function tcp_download_feed() {
 
-	if ( !get_option('tcp_gtfs_url') && !($_POST['alternate_feed'] == 'true') ) {
+	if ( ! get_option('tcp_gtfs_url') && ! ( $_POST['alternate_feed'] == 'true' ) ) {
 		return null;
 	}
 
@@ -178,25 +172,25 @@ function tcp_download_feed() {
 	// Erase all old files; will delete any custom uploaded files as well
 	array_map('unlink', glob( $feed_dir . '*.txt' ) );
 
-	if ( !file_exists( $feed_dir ) ) {
+	if ( ! file_exists( $feed_dir ) ) {
 		mkdir( $feed_dir, 0777, true );
 	}
 
 	// If using .zip direct upload, extract the zip to the feed Directory
-	if ($_POST['alternate_feed'] == 'true') {
+	if ( $_POST['alternate_feed'] == 'true' ) {
 		$tmp_path = $_FILES['gtfs_zip_input']['tmp_name'];
 
-		if ( !( $feed_download = @file_get_contents($tmp_path, true) ) ) {
+		if ( ! ( $feed_download = @file_get_contents( $tmp_path, true ) ) ) {
 			return null;
 		}
 	} else {
 		$gtfs_feed = esc_url( get_option('tcp_gtfs_url') );
 
-		if (!filter_var($gtfs_feed, FILTER_VALIDATE_URL)) {
+		if ( ! filter_var( $gtfs_feed, FILTER_VALIDATE_URL ) ) {
 			return null;
 		}
 
-		if ( !( $feed_download = @file_get_contents($gtfs_feed, true) ) ) {
+		if ( ! ( $feed_download = @file_get_contents( $gtfs_feed, true ) ) ) {
 			return null;
 		}
 	}
@@ -209,10 +203,10 @@ function tcp_download_feed() {
 		return null;
 	}
 	// Only copy over txt files and flatten directory structure
-	for($i = 0; $i < $zip->numFiles; $i++) {
-		$entry = $zip->getNameIndex($i);
-		if ( preg_match('#\.(txt)$#i', $entry) ) {
-			$fileinfo = pathinfo($entry);
+	for( $i = 0; $i < $zip->numFiles; $i++ ) {
+		$entry = $zip->getNameIndex( $i );
+		if ( preg_match('#\.(txt)$#i', $entry ) ) {
+			$fileinfo = pathinfo( $entry );
 			copy( "zip://" . $download_path . "#" . $entry, $feed_dir . $fileinfo['basename'] );
 		}
 	}
@@ -222,17 +216,18 @@ function tcp_download_feed() {
 
 function tcp_update_routes( $route_file ) {
 
-    $gtfs_data = array_map('str_getcsv', file($route_file));
-    $header = array_shift($gtfs_data);
-    array_walk($gtfs_data, '_combine_array', $header);
-	$route_ids = array_column($gtfs_data, 'route_id');
+    $gtfs_data = array_map( 'str_getcsv', file( $route_file ) );
+    $header    = array_shift( $gtfs_data );
+    array_walk( $gtfs_data, '_combine_array', $header );
+	$route_ids = array_column( $gtfs_data, 'route_id' );
+
 	// delete any existing route posts that are not in the new GTFS
 	$args = array(
 		'post_type'		=> 'route',
 		'numberposts'	=> -1,
 		'meta_key'		=> 'route_id',
-		'meta_value'		=> $route_ids,
-		'meta_compare'		=> 'NOT IN',
+		'meta_value'	=> $route_ids,
+		'meta_compare'	=> 'NOT IN',
 	);
 	$expired_routes = get_posts( $args );
 	foreach( $expired_routes as &$to_delete ) {
@@ -242,9 +237,9 @@ function tcp_update_routes( $route_file ) {
 
     foreach( $gtfs_data as $ind=>$route ) {
         // If route_long_name exists, use it as the default name for post title and name
-        $default_name = ($route['route_short_name'] == "") ? $route['route_long_name'] : 'Route ' . $route['route_short_name'];
-        $tag_name = tcp_route_url($default_name);
-        $route_id = $route['route_id'];
+        $default_name = ( $route['route_short_name'] == "") ? $route['route_long_name'] : 'Route ' . $route['route_short_name'];
+        $tag_name     = tcp_route_url( $default_name );
+        $route_id     = $route['route_id'];
 
 		//Check if the route post already exists. If not, create new route
 		$post_to_update_id = null;
@@ -279,7 +274,7 @@ function tcp_update_routes( $route_file ) {
         // Update route meta fields from GTFS data
         foreach ( $route as $key=>$value ) {
             if ( $key != "" ) {
-				update_post_meta($post_to_update_id, $key, $value);
+				update_post_meta( $post_to_update_id, $key, $value );
             }
 		}
 		
@@ -291,12 +286,12 @@ function tcp_update_routes( $route_file ) {
 }
 
 function tcp_route_url( $route_name ) {
-	$default_name = str_replace("-", " ", str_replace(" - ", " ", $route_name));
+	$default_name = str_replace("-", " ", str_replace( " - ", " ", $route_name ) );
 	if ( has_filter('tcp_filter_route_url') ) {
-		$default_name = trim(str_replace(" ", "-", strtolower($default_name)));
-		return apply_filters('tcp_filter_route_url', $default_name );
+		$default_name = trim( str_replace( " ", "-", strtolower( $default_name ) ) );
+		return apply_filters( 'tcp_filter_route_url', $default_name );
 	} else {
-		return trim(str_replace(" ", "-", strtolower($default_name)));
+		return trim( str_replace( " ", "-", strtolower($default_name ) ) );
 	}
 }
 
@@ -304,26 +299,27 @@ function tcp_update_timetables( $timetable_file ) {
 
 	// Check to see if we have timetables to upload
 	$upload_status = $_FILES['timetable_zip_input']['error'];
-	if ($upload_status == 0) {
+	if ( $upload_status == 0 ) {
 		tcp_upload_timetables();
 	}
 
 	// If files were attached but there is an error
-	if (($upload_status != 0) && ($upload_status != 4)) {
+	if ( ( $upload_status != 0 ) && ( $upload_status != 4 ) ) {
 		return null;
 	}
 
-    $gtfs_data = array_map('str_getcsv', file($timetable_file));
-    $header = array_shift($gtfs_data);
-    array_walk($gtfs_data, '_combine_array', $header);
-	$timetable_ids = array_column($gtfs_data, 'timetable_id');
+    $gtfs_data = array_map( 'str_getcsv', file($timetable_file ) );
+    $header    = array_shift( $gtfs_data );
+    array_walk( $gtfs_data, '_combine_array', $header );
+	$timetable_ids = array_column( $gtfs_data, 'timetable_id' );
+
 	// delete any existing timetables that are not in the new GTFS
 	$args = array(
 		'post_type'		=> 'timetable',
 		'numberposts'	=> -1,
 		'meta_key'		=> 'timetable_id',
-		'meta_value'		=> $timetable_ids,
-		'meta_compare'		=> 'NOT IN',
+		'meta_value'    => $timetable_ids,
+		'meta_compare'  => 'NOT IN',
 	);
 	$expired_timetables = get_posts( $args );
 	foreach( $expired_timetables as &$to_delete ) {
@@ -339,8 +335,8 @@ function tcp_update_timetables( $timetable_file ) {
 			$timetable['thursday'], $timetable['friday'], $timetable['saturday'], $timetable['sunday']
 		);
 		$timetable['days_of_week'] = $days_of_week;
-		$timetable_name = tcp_timetable_name( $timetable );
-		$tag_name = str_replace(" ", "_", strtolower($timetable_name));
+		$timetable_name            = tcp_timetable_name( $timetable );
+		$tag_name                  = str_replace( " ", "_", strtolower( $timetable_name ) );
 
 		// Find out if content exists in timetables folder
 		// TODO replace with more robust, easy to use utility
@@ -352,7 +348,7 @@ function tcp_update_timetables( $timetable_file ) {
 		if ( file_exists( $timetable_dir ) ) {
 			// Locate by timetable ID, hypothetically there should never be more than 1
 			foreach( glob( $timetable_dir . $timetable['timetable_id'] . "_*.html") as $timetable_file ) {
-				$content = file_get_contents($timetable_file, true);
+				$content = file_get_contents( $timetable_file, true );
 			}
 		}
 
@@ -390,7 +386,7 @@ function tcp_update_timetables( $timetable_file ) {
         // Update route meta fields from GTFS data
         foreach ( $timetable as $key=>$value ) {
             if ( $key != "" ) {
-                update_post_meta($post_to_update_id, $key, $value);
+                update_post_meta( $post_to_update_id, $key, $value );
             }
 		}
 		
@@ -416,12 +412,12 @@ function tcp_upload_timetables() {
 		$timetable_dir = apply_filters( 'timetable_directory', $timetable_dir );
 	}
 
-	if ( !file_exists( $timetable_dir ) ) {
+	if ( ! file_exists( $timetable_dir ) ) {
 		mkdir( $timetable_dir, 0777, true );
 	} else {
 		array_map('unlink', glob( $timetable_dir . '*.html' ) );
 	}
-	$tmp_path = $_FILES['timetable_zip_input']['tmp_name'];
+	$tmp_path      = $_FILES['timetable_zip_input']['tmp_name'];
 	$download_path = $timetable_dir . 'timetables.zip';
 	$file_download = @file_get_contents( $tmp_path, true );
 	file_put_contents( $download_path, $file_download );
@@ -431,9 +427,9 @@ function tcp_upload_timetables() {
 		return null;
 	}
 	// Only copy over HTML files and flatten directory structure
-	for($i = 0; $i < $zip->numFiles; $i++) {
+	for( $i = 0; $i < $zip->numFiles; $i++ ) {
 		$entry = $zip->getNameIndex($i);
-		if ( preg_match('#\.(html)$#i', $entry) ) {
+		if ( preg_match('#\.(html)$#i', $entry ) ) {
 			$fileinfo = pathinfo($entry);
 			copy( "zip://" . $download_path . "#" . $entry, $timetable_dir . $fileinfo['basename'] );
 		}
@@ -457,8 +453,8 @@ function tcp_get_status_message( $code ) {
 		'200' => 'GTFS Update Success. Please ensure <strong>Routes</strong> contain correct information.',
 		'201' => 'GTFS Update Success. Please ensure <strong>Routes</strong> contain correct information. No timetables.txt present (timetables not updated).',
 	);
-	if ($codes[$code]) {
-		return $codes[$code];
+	if ( $codes[ $code ] ) {
+		return $codes[ $code ];
 	} else {
 		return $code;
 	}
@@ -480,20 +476,20 @@ function tcp_timetable_days( $timetable ) {
 		$days_of_week = 'Mon-Thurs';
 	} else {
 		$timetable_days = array();
-		foreach ($days as $day) {
-			if ($timetable[$day]) {
-				array_push($timetable_days, ucfirst($day));
+		foreach ( $days as $day ) {
+			if ( $timetable[ $day ] ) {
+				array_push( $timetable_days, ucfirst( $day ) );
 			}
 		}
-		if (count($timetable_days) == 1) {
+		if ( count( $timetable_days ) == 1 ) {
 			$days_of_week = $timetable_days[0];
 		} else {
 			$idx = 0;
-			while ($idx < count($timetable_days)) {
-				$days_of_week .= $timetable_days[$idx] . ', ';
+			while ( $idx < count( $timetable_days ) ) {
+				$days_of_week .= $timetable_days[ $idx ]  . ', ';
 				$idx++;
 			}
-			$days_of_week .= $timetable_days[$idx];
+			$days_of_week .= $timetable_days[ $idx ] ;
 		}
 	}
 	if ( has_filter('tcp_timetable_filter_days_of_week') ) {
@@ -504,6 +500,6 @@ function tcp_timetable_days( $timetable ) {
 }
 
 // Array combine solution from dejiakala@gmail.com
-function _combine_array(&$row, $key, $header) {
-  $row = array_combine($header, $row);
+function _combine_array( &$row, $key, $header ) {
+  $row = array_combine( $header, $row );
 }
