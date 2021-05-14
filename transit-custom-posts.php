73c718bdf841566353bd1d77be45af3ee6ab3d6d
Chance Corbeil <corbeilchance@gmail.com>
2021-05-14 14:07:05 -0700

    Update version number to 3.0

--- transit-custom-posts.php
+++ transit-custom-posts.php
@@ -3,7 +3,7 @@
 * Plugin Name: Transit Custom Posts
 * Plugin URI: https://trilliumtransit.github.io/transit-custom-posts/
 * Description: Creates route, alert, timetable, and other custom post types used for transit sites. Programmatically updates data from a GTFS feed.
-* Version: 2.0
+* Version: 3.0
 * Author: NomeQ
 * Author URI: https://trilliumtransit.com/
 * License: GPL2
